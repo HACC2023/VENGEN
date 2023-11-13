088c0d32ca95ecd04877cae8660cbb5ae1f1dc19
Ethan Chee <61472206+ethancheez@users.noreply.github.com>
2023-11-13 02:36:57 -1000

    Fix homepage huge padding bug

--- app/imports/ui/components/HomeThreadItem.jsx
+++ app/imports/ui/components/HomeThreadItem.jsx
@@ -9,7 +9,7 @@ const HomeThreadItem = ({ thread }) => (
     <a href={`/thread/${thread._id}`} style={{ textDecoration: 'none' }}>
       <Row className="mb-2 align-items-left" style={{ textAlign: 'left' }}>
         <Image src="/images/pfp.jpg" roundedCircle style={{ width: '4em' }} />
-        <p style={{ position: 'absolute', paddingLeft: '4em' }}>{thread.owner} | {thread.messages[0].time.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })}</p>
+        <p style={{ marginTop: '-2.5em', paddingLeft: '4em' }}>{thread.owner} | {thread.messages[0].time.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })}</p>
         <p className="mt-2 mb-0" style={{ fontSize: '20px', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }}><strong>{thread.title}</strong></p>
         <p className="mb-1" style={{ wordWrap: 'break-word', lineHeight: 'normal' }}>{thread.messages[0].message.length > 500 ? `${thread.messages[0].message.substring(0, 500)}...` : thread.messages[0].message}</p>
         <Row>
